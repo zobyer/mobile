80d46941f306ef7953de61c84046cee482a9b2f7
zobyer <ahmzobyer@gmail.com>
2020-07-27 12:07:17 -0700

    2nd commit

--- mobile/settings.py
+++ mobile/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nf%0%@1kfpiy#$^pwwk!08-p0kpe*8z6)%zop#ayzuu_cf3!bg'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
